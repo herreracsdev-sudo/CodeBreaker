88da0b6e649d7c7e169d1630499e639cc270bc15
Daniel Herrera <32147322+Djherrera1@users.noreply.github.com>
2026-06-01 16:46:44 -0700

    Initial Commit

---
+++ CodeBreakerTests/CodeBreakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerTests.swift
+//  CodeBreakerTests
+//
+//  Created by Daniel Herrera on 2/3/26.
+//
+
+import Testing
+@testable import CodeBreaker
+
+struct CodeBreakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
